abcc380f7b74560cb7dbd958167cd5957e7eef5e
Aki Sasaki <aki@escapewindow.com>
2016-06-29 16:56:13 -0700

    28% coverage

--- tests/test_reqhash.py
+++ tests/test_reqhash.py
@@ -2,6 +2,24 @@
 
 import pytest
 import reqhash
+import subprocess
 
-def test_foo():
-    assert 1
+
+def test_die():
+    with pytest.raises(SystemExit):
+        reqhash.die("foo")
+
+
+def test_usage():
+    with pytest.raises(SystemExit):
+        reqhash.usage()
+
+
+def test_run_cmd_success():
+    val = reqhash.run_cmd("echo")
+    assert val == 0
+
+
+def test_run_cmd_failure():
+    with pytest.raises(subprocess.CalledProcessError):
+        reqhash.run_cmd(['bash', '-c', 'exit 1'])

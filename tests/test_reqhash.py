9a7b0fe02ff96e228fc1fbf8d3c326658d5396d9
Aki Sasaki <aki@escapewindow.com>
2016-06-29 17:49:51 -0700

    add get_output tests

--- tests/test_reqhash.py
+++ tests/test_reqhash.py
@@ -6,16 +6,24 @@ import reqhash
 import six
 import subprocess
 
-TO_STR_INPUT = [
+# params {{{1
+TO_STR_PARAMS = [
     (b'asdf', 'asdf'),
     (u'asdf', u'asdf'),
 ]
 TO_STR_IDS = ["binary", "unicode"]
 if six.PY3:
-    TO_STR_INPUT.append((u'Hello, \U0001F4A9!'.encode('utf-8'), u'Hello, \U0001F4A9!'))
+    TO_STR_PARAMS.append((u'Hello, \U0001F4A9!'.encode('utf-8'), u'Hello, \U0001F4A9!'))
     TO_STR_IDS.append("poo")
 
+GET_OUTPUT_PARAMS = [
+    (["echo", "foo"], "foo"),
+    (["bash", "-c", "echo bar && >&2 echo foo"], "bar"),
+]
+GET_OUTPUT_IDS = ["stdout", "stdout+stderr"]
 
+
+# die, usage {{{1
 def test_die():
     with pytest.raises(SystemExit):
         reqhash.die("foo")
@@ -26,6 +34,7 @@ def test_usage():
         reqhash.usage()
 
 
+# run_cmd {{{1
 def test_run_cmd_success():
     val = reqhash.run_cmd("echo")
     assert val == 0
@@ -36,7 +45,20 @@ def test_run_cmd_failure():
         reqhash.run_cmd(['bash', '-c', 'exit 1'])
 
 
-@pytest.mark.parametrize("to_str_input", TO_STR_INPUT, ids=TO_STR_IDS)
-def test_to_str(to_str_input):
-    val = reqhash.to_str(to_str_input[0])
-    assert val == to_str_input[1]
+# to_str {{{1
+@pytest.mark.parametrize("params", TO_STR_PARAMS, ids=TO_STR_IDS)
+def test_to_str(params):
+    val = reqhash.to_str(params[0])
+    assert val == params[1]
+
+
+# get_output {{{1
+@pytest.mark.parametrize("params", GET_OUTPUT_PARAMS, ids=GET_OUTPUT_IDS)
+def test_get_output(params):
+    output = reqhash.get_output(params[0])
+    assert output.rstrip() == params[1]
+
+
+def test_get_output_error():
+    with pytest.raises(subprocess.CalledProcessError):
+        reqhash.get_output(["bash", "-c", "echo foo && exit 1"])

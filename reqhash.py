3bfde69e01e8067da78ddb0b1bebf9c837ae9d6b
Aki Sasaki <aki@escapewindow.com>
2016-07-02 16:07:14 -0700

    remove pip_main import

--- reqhash.py
+++ reqhash.py
@@ -4,7 +4,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import absolute_import, division, print_function
 import os
-from pip import main as pip_main
 import pprint
 import re
 import shutil
